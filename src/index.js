e65969ee172b55a74f627cc054668d322f1575ba
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-09-04 23:26:17 -0400

    fix: remove console log

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ const getMochaContext = () => cy.state('runnable').ctx
 const skipIfPreviousTestsFailed = () => {
   const ctx = getMochaContext()
   const thisTestIndex = ctx.test.parent?.tests?.indexOf(ctx.test)
-  console.log(thisTestIndex)
   const previousTests = ctx.test.parent?.tests?.slice(
     0,
     thisTestIndex,
   )
-  console.log(previousTests)
   const anyFailedTests = previousTests?.some(
     (test) => test.state === 'failed',
   )
-  console.log({ thisTestIndex, anyFailedTests })
   if (anyFailedTests) {
     return ctx.skip()
   }
